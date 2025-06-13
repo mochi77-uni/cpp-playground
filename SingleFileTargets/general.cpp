d519ac34de5f31afcced43b2a4c8d579d0081631
mochi77-uni <mochi77_uni@gmail.com>
2025-06-13 15:13:10 +0800

    remove test.c and clear general.cpp content for temporary testing

--- SingleFileTargets/general.cpp
+++ SingleFileTargets/general.cpp
@@ -1,13 +1,7 @@
 
 #include <iostream>
-#include <string>
-
-std::string f() {
-	return "hello world";
-}
 
 int main() {
-	std::string_view sv = f();
-	std::cout << sv << std::endl;
+
 	return 0;
 }
