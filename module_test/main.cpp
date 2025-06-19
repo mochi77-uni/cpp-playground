63609c25cc3c090dceee00904e6f521660dc1459
mochi77-uni <mochi77_uni@gmail.com>
2025-06-19 17:30:50 +0800

    add module_test

---
+++ module_test/main.cpp
@@ -0,0 +1,10 @@
+
+import math;
+#include <iostream>
+
+int main() {
+	std::cout << math::add(1, 2) << std::endl;
+	std::cout << math::subtract(1, 2) << std::endl;
+
+	return 0;
+}
---
+++ module_test/math.cppm
@@ -0,0 +1,14 @@
+
+module;
+
+export module math;
+
+export namespace math {
+	int add(int a, int b) {
+		return a + b;
+	}
+
+	int subtract(int a, int b) {
+		return a - b;
+	}
+}
